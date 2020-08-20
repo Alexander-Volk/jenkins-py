fcb4eb48b66dc2cbe12557bb57ec2b6688ccd5de
Alexander-Volk <mr.vol4onok@gmail.com>
2020-08-20 17:49:09 +0300

    test deployment of the application version 2.1

--- application.py
+++ application.py
@@ -22,21 +22,12 @@ welcome = """
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-  <!--
-    Copyright 2012 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-
-    Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
-
-        http://aws.Amazon/apache2.0/
-
-    or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
-  -->
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
   <title>Welcome</title>
   <style>
   body {
     color: #ffffff;
-    background-color: #E0E0E0;
+    background-color: #d7d7d9;
     font-family: Arial, sans-serif;
     font-size:14px;
     -moz-transition-property: text-shadow;
@@ -64,7 +55,6 @@ welcome = """
     right: 50%;
     bottom: 0px;
     left: 0px;
-
     text-align: right;
     padding-top: 11em;
     background-color: #1BA86D;
@@ -75,16 +65,17 @@ welcome = """
     width: 75%;
     float:right;
   }
+  /*.textColumn h1 {*/
+  /*    color: red;*/
+  /*}*/
   .linksColumn {
     position: absolute;
     top:0px;
     right: 0px;
     bottom: 0px;
     left: 50%;
-
     background-color: #E0E0E0;
   }
-
   h1 {
     font-size: 500%;
     font-weight: normal;
@@ -106,12 +97,12 @@ welcome = """
 </head>
 <body id="sample">
   <div class="textColumn">
-    <h1><font color=red>Congratulations <br> <font color=purple>DevOps Engineer</font></h1>
+      <h1><font color="red">Congratulations</font> <br> <font color="purple">DevOps Engineer!!!</font></h1>
     <p>Your first AWS Elastic Beanstalk Python Application is now running on your own dedicated environment in the AWS Cloud</p>
     <p>This environment is launched with Elastic Beanstalk Python Platform</p>
   </div>
-  
-  <div class="linksColumn"> 
+
+  <div class="linksColumn">
     <h2>What's Next?</h2>
     <ul>
     <li><a href="http://docs.amazonwebservices.com/elasticbeanstalk/latest/dg/">AWS Elastic Beanstalk overview</a></li>
@@ -120,10 +111,9 @@ welcome = """
     <li><a href="http://docs.amazonwebservices.com/elasticbeanstalk/latest/dg/create_deploy_Python_flask.html">Deploy a Flask Application to AWS Elastic Beanstalk</a></li>
     <li><a href="http://docs.amazonwebservices.com/elasticbeanstalk/latest/dg/create_deploy_Python_custom_container.html">Customizing and Configuring a Python Container</a></li>
     <li><a href="http://docs.amazonwebservices.com/elasticbeanstalk/latest/dg/using-features.loggingS3.title.html">Working with Logs</a></li>
-
     </ul>
-    <h2>Jenkins-Python</h2>
-    <img src="image.png">
+      <h2><font color="#2f4e60">Jen</font><font color="#d33530">k</font><font color="#2f4e60">ins</font>-<font color="#4f67be">Pyt</font><font color="#f8bb03">hon</font></h2>
+      <img src="image.jpg">
   </div>
 </body>
 </html>
